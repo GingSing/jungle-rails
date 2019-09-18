19e02723ae89078cb453ab933f333d6a69ae2ab7
Gary <garyyue2017@gmail.com>
2019-09-18 21:58:23 +0000

    added information on amount of products and categories in admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   include AuthenticationConcern
   
   def show
+    @products_count = Product.all.count
+    @categories_count = Category.all.count
   end
 end
